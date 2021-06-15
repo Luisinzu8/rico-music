6530f9c98572dcd8238019960aede34170a82f3f
Luis Inzunza <luisinzu08@gmail.com>
2021-06-15 12:52:01 -0700

    react template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Rico Music Page
+      </header>
+    </div>
+  );
+}
+
+export default App;
